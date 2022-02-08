b0eaa8cc91c1a28147cefda8782aed2c3b93ed70
terremou <terrence_mou@foxmail.com>
2022-02-08 23:30:52 +0800

    完成笔记本页面的交互功能：展示笔记本列表，添加、删除、修改

--- src/components/NotebookList.vue
+++ src/components/NotebookList.vue
@@ -2,40 +2,26 @@
   <div id="notebook-list">
     <header>
       <h2>笔记本</h2>
-      <Button size="small" theme="text">
+      <Button size="small" theme="text" @click.native="onCreate">
         <i class="iconfont icon-plus"/>新建笔记本
       </Button>
     </header>
     <main>
       <div class="layout">
-        <h3>笔记本列表(10)</h3>
+        <h3>笔记本列表({{ notebooks.length }})</h3>
         <div class="list">
-          <a href="#">
-            <div class="item">
-              <div class="title">
-                <span class="iconfont icon-notebook"/>笔记本标题1
-                <span>3</span>
-              </div>
-              <div class="actions">
-                <span>3天前</span>
-                <span>编辑</span>
-                <span>删除</span>
-              </div>
+          <router-link to="/note/1" class="item"
+                       v-for="notebook in notebooks" :key="notebook.id">
+            <div class="title">
+              <span class="iconfont icon-notebook"/>{{ notebook.title }}
+              <span>{{ notebook.noteCounts }}</span>
             </div>
-          </a>
-          <a href="#">
-            <div class="item">
-              <div class="title">
-                <span class="iconfont icon-notebook"/>笔记本标题2
-                <span>1</span>
-              </div>
-              <div class="actions">
-                <span>15天前</span>
-                <span>编辑</span>
-                <span>删除</span>
-              </div>
+            <div class="actions" @click.stop.prevent>
+              <span>{{ notebook.createdAt }} 3天前</span>
+              <span @click="onEdit(notebook)">编辑</span>
+              <span @click="onDelete(notebook)">删除</span>
             </div>
-          </a>
+          </router-link>
         </div>
       </div>
     </main>
@@ -47,13 +33,16 @@ import Auth from '@/apis/auth';
 import Notebook from '@/apis/notebookApis';
 import Button from '@/components/Button';
 
-window.notebook = Notebook
+/** @namespace notebook.noteCounts **/
+/** @namespace notebook.createdAt **/
 
 export default {
   name: 'NotebookList',
   components: {Button},
   data() {
-    return {};
+    return {
+      notebooks: [],
+    };
   },
 
   created() {
@@ -62,6 +51,45 @@ export default {
         this.$router.push({path: '/login'});
       }
     });
+    Notebook.getNotebookList().then((res) => {
+      this.notebooks = res.data;
+    });
+  },
+
+  methods: {
+    onCreate() {
+      const title = window.prompt('创建笔记本');
+      if (title === null) {
+        return;
+      }
+      if (title.trim() === '') {
+        return alert('笔记本名不能为空');
+      }
+      Notebook.addNotebook({title}).then(res => {
+        console.log(res);
+        this.notebooks.unshift(res.data);
+        alert(res.msg);
+      });
+    },
+    onEdit(notebook) {
+      const title = window.prompt('修改笔记本名', notebook.title);
+      Notebook.updateNotebook(notebook.id, {title}).then(res => {
+        console.log(res);
+        notebook.title = title;
+        alert(res.msg);
+      });
+    },
+    onDelete(notebook) {
+      const isConfirm = window.confirm('你确定要删除吗？');
+      if (isConfirm) {
+        Notebook.deleteNotebook(notebook.id).then(res => {
+          console.log(res);
+          const index = this.notebooks.indexOf(notebook);
+          this.notebooks.splice(index, 1);
+          alert(res.msg);
+        });
+      }
+    }
   },
 
 };
