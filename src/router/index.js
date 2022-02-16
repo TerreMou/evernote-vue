c498a57ab2d79b84fba743fff532831b397c2a3b
terremou <terrence_mou@foxmail.com>
2022-02-16 11:33:36 +0800

    模块懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Login from '@/components/Login';
-import NotebookList from '@/components/NotebookList';
-import NoteDetail from '@/components/NoteDetail';
-import TrashDetail from '@/components/TrashDetail';
 
 Vue.use(Router);
 
@@ -16,19 +12,19 @@ export default new Router({
     {
       path: '/login',
       name: 'Login',
-      component: Login
+      component: () => import('../components/Login.vue')
     },
     {
       path: '/notebooks',
-      component: NotebookList
+      component: () => import('../components/NotebookList.vue')
     },
     {
       path: '/note',
-      component: NoteDetail
+      component: () => import('../components/NoteDetail.vue')
     },
     {
       path: '/trash',
-      component: TrashDetail
+      component: () => import('../components/TrashDetail.vue')
     },
   ]
 });
